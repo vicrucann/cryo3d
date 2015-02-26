58c97c884ee095e573c69c61945890df9266bba4
Victoria Rudakova <victoria.rudakova@yale.edu>
2015-02-26 10:11:49 -0500

    cache prototype edited to account for data type

--- test/bigdata.m
+++ test/bigdata.m
@@ -4,34 +4,36 @@
 clear;
 clc;
 
-gb = 0.9;
+gb = 0.9; % goal precision in Gb
 nimg = 1000;
 npro = 300;
 nrot = 360;
 ntra = 150;
+type = 'single';
+stype = 4; % sizeof(single);
 
 dr = nrot;
-dt = floor(gb*1024^3/(4*nimg*npro*nrot));
+dt = floor(gb*1024^3/(stype*nimg*npro*nrot));
 if (dt == 0)
     % most probably will not happen, otherwise will have to split along rotation
     dt = 1;
-    dr = floor(gb*1024^3/(4*nimg*npro));
+    dr = floor(gb*1024^3/(stype*nimg*npro));
 end
 
 tic;
-chunk = rand(nimg, npro, dr, dt);
+chunk = rand(nimg, npro, dr, dt, type);
 toc;
 
 % allocate memory by fwrite
 tic;
-pathcache = '/home/cryo-em-3d/cryo3d/test/cache/';
+pathcache = 'cache\';
 fname = '_.dat';
 fid = fopen([pathcache fname], 'Wb');
-fwrite(fid, chunk, 'double');
+fwrite(fid, chunk, type);
 fclose(fid);
 toc;
 
-m = memmapfile([pathcache fname], 'Format', 'double',...
+m = memmapfile([pathcache fname], 'Format', type,...
         'Writable', true);
     
 fprintf('loop of reading and writing:\n');
@@ -43,7 +45,7 @@ for i = 1:10
     fprintf('matrix equality: %i\n', isequal(data,chunk));
     
     tic;
-    chunk = rand(nimg, npro, dr, dt);
+    chunk = rand(nimg, npro, dr, dt, type);
     m.Data = chunk;
     toc;
 end
