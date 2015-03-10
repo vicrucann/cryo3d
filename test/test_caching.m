99b5d741236ad95083b7b30244c12ef89c80ae15
Victoria Rudakova <vicrucann@gmail.com>
2015-03-10 13:27:47 -0400

    test folder: up to date

--- test/test_caching.m
+++ test/test_caching.m
@@ -5,23 +5,22 @@
 
 num_chunks = 20;
 type = 'single';
-carr = create_cached_array([1000, 300, 360, 150], 'cache', type, num_chunks);
-for i = 1:2
+carr = create_cached_array([1000, 300, 360, 150], 'cache', type, num_chunks, 3, 1);
+for i = 1:num_chunks
     tic;
     chunk = rand(1000,300,18,150, type); % break in 3rd dimension
     fprintf('done random matrix allocation\n');
     toc;
     
     tic;
-    write_cached_array_chunk(carr, chunk, i);
+    carr = write_cached_array_chunk(carr, chunk, i);
     fprintf('done writing to cache of chunk %i\n', i);
     toc;
     
     tic;
-    chunk_r = read_cached_array_chunk(carr, i, [1000, 300, 18, 150]);
+    chunk_r = read_cached_array(carr, [0, 0, i*18, 150]);
     fprintf('done reading chunk %i from a cache\n', i);
     toc;
-    fprintf('matrix equality: %i\n\n', isequal(chunk,chunk_r));    
 end
 
 
--- test/write_cached_array_chunk.m
+++ test/write_cached_array_chunk.m
@@ -1,13 +1,18 @@
-function passed = write_cached_array_chunk(cacharr, chunk, idx_chunk)
+function cacharr = write_cached_array_chunk(cacharr, chunk, idx_chunk)
 
-passed = 0;
-fprintf('writing the cached file...');
-tic;
-fname = [num2str(idx_chunk) '.dat'];
-fid = fopen([cacharr.path fname], 'Wb');
-fwrite(fid, chunk, cacharr.type);
-fclose(fid);
-fprintf('done\n');
-toc;
-fprintf('\n');
-passed = 1;
+if (cacharr.caching == 1)
+    fname = [num2str(idx_chunk) '.dat'];
+    fid = fopen([cacharr.path fname], 'Wb');
+    fwrite(fid, chunk, cacharr.type);
+    fclose(fid);
+    if (idx_chunk ==  1)
+        cacharr.data = chunk; % prepare data for the first use
+    end
+else
+    batchsize = ceil(cacharr.dimensions(cacharr.broken) / cacharr.nchunks);
+    if (idx_chunk < cacharr.nchunks)
+        cacharr.data(:,:,batchsize*(idx_chunk-1)+1:batchsize*idx_chunk,:) = chunk;
+    else
+        cacharr.data(:,:,batchsize*(idx_chunk-1)+1:end,:) = chunk;
+    end
+end
