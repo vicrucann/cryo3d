dda2c33ae931470b1aa66258240967f058a3bb39
Victoria Rudakova <victoria.rudakova@yale.edu>
2015-03-10 13:27:47 -0400

    test folder: up to date

--- test/create_cached_array.m
+++ test/create_cached_array.m
@@ -1,7 +1,14 @@
-function cacharr = create_cached_array(size, path_cache, type, num_chunks)
+function cacharr = create_cached_array(size, path_cache, type, num_chunks, idx_broken, caching)
 
 if ~exist(path_cache)
     mkdir(path_cache);
 end
 
-cacharr = struct('dimensions', size, 'path', [path_cache '\'], 'type', type, 'nchunks', num_chunks);
+if (caching == 0)
+    data = zeros(size, type);
+else
+    data = 0;
+end
+
+cacharr = struct('dimensions', size, 'path', [path_cache '\'], 'type', type, 'nchunks', num_chunks, 'broken', idx_broken, ...
+    'caching', caching, 'data', data, 'currchunk', 1);
