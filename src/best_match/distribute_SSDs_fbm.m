7c5cbb503c36b82b14b3b6eb74e8947eee140113
Victoria Rudakova <vicrucann@gmail.com>
2015-05-27 14:40:08 -0400

    remmat should be an insider variable

--- src/best_match/distribute_SSDs_fbm.m
+++ src/best_match/distribute_SSDs_fbm.m
@@ -1,6 +1,7 @@
 function [projinds,rotinds,SSDs,transinds,scales] = ...
     distribute_SSDs_fbm(projnorms,projcoeffs,imcoeffs,ips,ctfinds,numim,numctf,numproj,numrot,searchtrans,imnorms,maxmem,...
-    ipaddrs,login,ppath,remmat,varmat,sleeptime,resfold,printout)
+    ipaddrs,login,ppath,varmat,sleeptime,resfold,printout)
 
 [ncluster ~] = find(ipaddrs==' ');
 ncluster = size(ncluster,2)+1;
+remmat = 'comp_SSDs_fast_best_match';
