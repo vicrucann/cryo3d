aad498456457df6886ad625d5c8555dcc2ccba84
Victoria Rudakova <vicrucann@gmail.com>
2015-08-19 16:21:05 -0400

    mex_proj signatures fixed, Eig vector calculation fixed

--- src/postprocessing/test_fourier.m
+++ src/postprocessing/test_fourier.m
@@ -1,11 +1,13 @@
 % Script to test post-processing part of cryo3d
 %% Additional libraries
 
-addpath('\cryoEm');
-addpath('\cryoEm\EMBase');
+addpath('cryoEm');
+addpath('cryoEm\EMBase');
 addpath('cryoEm\EMIO');
 addpath('cryoEm\EMSpec');
 addpath('cryoEm\GriddingLib');
+addpath('other');
+addpath('data');
 
 %% The input variables
 
@@ -14,10 +16,10 @@ addpath('cryoEm\GriddingLib');
 % defocus
 % pixA, lambda
 
-%load('defocus.mat');
+load('defocus.mat');
 %load('fbm_aligned_ims.mat');
-%load('eigStruct.mat');
-%load('coord_axes_12.mat');
+load('eigStruct.mat');
+load('coord_axes_6.mat');
 %im_projinds = projinds;
 
 lambda = EWavelength(300);
@@ -25,7 +27,8 @@ pixA = 1.32; % obtained by using ReadMRC
 
 %% Calculate mu
 
-% % simulate mu
+%=======
+% simulate mu
 % mx = max(im_projinds);
 % mu = zeros(size(aligned_ims,1), size(aligned_ims,2), mx);
 % mu_projinds = zeros(mx,1);
@@ -35,21 +38,25 @@ pixA = 1.32; % obtained by using ReadMRC
 %     mu(:,:,j)=aligned_ims(:,:,k1);
 %     mu_projinds(j) = k1;
 % end
+%=======
 
-eigVal1 = norm(eigStruct.eigs3d(:,:,:,1));
-eigVal2 = norm(eigStruct.eigs3d(:,:,:,2));
+eig1 = eigStruct.eigs3d(:,:,:,1);
+eig2 = eigStruct.eigs3d(:,:,:,2);
+eigVal1 = norm(eig1(:));
+eigVal2 = norm(eig2());
 eigVec1 = eigStruct.eigs3d(:,:,:,1)./eigVal1;
 eigVec2 = eigStruct.eigs3d(:,:,:,2)./eigVal2;
-n1=normrnd(0, eigVal1);
-n2=normrnd(0, eigVal2);
-M=size(eigStruct.meanVol, 3);
-rMax=floor(M/2-2);
-%mu = zeros(M,M,size(eigStruct.eigsProj, 3));
-%for k=1:size(eigStruct.eigsProj, 3)
- mu = mex_forward_project(eigStruct.meanVol, size(eigStruct.meanVol), coordAxes, size(eigStruct.eigsProj, 3), rMax)...
-        + n1 * mex_forward_project(eigVec1, size(eigVec1), coordAxes, size(eigStruct.eigsProj, 3), rMax)...
-        + n2 * mex_forward_project(eigVec2, size(eigVec2), coordAxes, size(eigStruct.eigsProj, 3), rMax);
-%end
+n1 = normrnd(0, eigVal1);
+n2 = normrnd(0, eigVal2);
+M = size(eigStruct.meanVol, 3);
+rMax = floor(M/2-2);
+num_projdir = size(eigStruct.eigsProj, 3);
+
+mu_mean = mex_forward_project(eigStruct.meanVol, M, coord_axes, num_projdir, rMax);
+mu_vec1 = n1 * mex_forward_project(eigVec1, size(eigVec1), coord_axes, size(eigStruct.eigsProj, 3), rMax);
+mu_vec2 = n2 * mex_forward_project(eigVec2, size(eigVec2), coord_axes, size(eigStruct.eigsProj, 3), rMax);
+
+mu = mu_mean + mu_vec1 + mu_vec2;
 
 %% Get the norms
 
